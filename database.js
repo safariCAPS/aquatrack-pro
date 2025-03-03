c832fa764ae6a5abfd93b8dfa53d9b63d0dcbc70
safariCAPS <150255144+safariCAPS@users.noreply.github.com>
2025-03-03 09:11:16 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  'https://tecenaxizjatwjgkgslt.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlY2VuYXhpemphdHdqZ2tnc2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3NTA0MjksImV4cCI6MjA1NjMyNjQyOX0.C5aPf3iv5UouM8-VN9HrE5ClM1tredCOyV7T00AGj8g'
+);
 // Submit Report
 document.getElementById('reportForm')?.addEventListener('submit', async (e) => {
   e.preventDefault();
